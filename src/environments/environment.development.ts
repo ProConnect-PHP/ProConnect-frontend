1154555477fdacbd647e5f110c3dd0747deae93c
gyabisito <josecitohernandez5@gmail.com>
2026-06-02 00:08:01 -0300

    Mapa

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8000/api/v1',
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiZ3lhYmlzaXRvIiwiYSI6ImNtcG91eWx4aDA0azYycW9wYm4xbnY5MnMifQ.0mEU1JxzXdiRucMhLQmuDA',
+    accessToken: 'pk.eyJ1IjoiZ3lhYmlzaXRvIiwiYSI6ImNtcG91czhlNDA4NWcycnB5cXo5cmZtNzUifQ.Xr-oAMmAwYGU6aO0wBi5Nw',
     defaultCenter: {
       latitude: -34.9011,
       longitude: -56.1645,
